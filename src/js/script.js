ff8a5cfd534a394e5177c2a1175c2f26938175ab
alnewlife <silver270@mail.ru>
2021-04-11 11:28:53 +0300

    add modal windows

--- src/js/script.js
+++ src/js/script.js
@@ -41,8 +41,22 @@ $(document).ready(function () {
 
    });
    (jQuery);
+
+   //Modal
+
+   $('[data-modal=consultation]').on('click', function () {
+      $('.overlay, #consultation').fadeIn('slow');
+   });
+   $('.modal__close').on('click', function () {
+      $('.overlay, #consultatuon, #thanks, #order').fadeOut('slow');
+   });
+
+   $('.button_mini').each(function (i) {
+      $(this).on('click', function () {
+         $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+         $('.overlay, #order').fadeIn('slow');
+      })
+
+   });
+
 });
-
-
-
-
