35ab1b8e7a0a3965c96c0020146ef872742f8eac
alnewlife <silver270@mail.ru>
2021-04-11 23:09:30 +0300

    add validation site, upscroll

--- src/js/script.js
+++ src/js/script.js
@@ -56,7 +56,63 @@ $(document).ready(function () {
          $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
          $('.overlay, #order').fadeIn('slow');
       })
-
    });
 
+
+   function validateForms(form) {
+      $(form).validate({
+         rules: {
+            name: "required",
+            phone: "required",
+            email: {
+               required: true,
+               email: true
+            }
+         },
+         messages: {
+            name: "Пожалуйста, введите Ваше имя",
+            phone: "Пожалуйста, введите номер телефона",
+            email: {
+               required: "Пожалуйста, введите свою почту",
+               email: "Неправильно введен адрес почты"
+            }
+         }
+      });
+   };
+
+   validateForms('#consultation form');
+   validateForms('#consultation-form');
+   validateForms('#order form');
+
+   $('input[name=phone]').mask("+375 (99) 999-99-99");
+
+   $('form').submit(function (e) {
+      e.preventDefault();
+      $.ajax({
+         type: "POST",
+         url: "mailer/smart.php",
+         data: $(this).serialize()
+      }).done(function () {
+         $(this).find("input").val("");
+         $('#consultation, #order').fadeOut();
+         $('.overlay, #thanks').fadeIn('slow');
+
+         $('form').trigger('reset');
+      });
+      return false;
+   });
+
+   $(window).scroll(function () {
+      if ($(this).scrollTop() > 1600) {
+         $('.pageup').fadeIn();
+      } else {
+         $('.pageup').fadeOut();
+      }
+   });
+
+   $("a[href^=#up]").click(function () {
+      const _href = $(this).attr("href");
+      $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+      return false;
+   });
 });
